1bb0e4a3bd3ae376305e24d320c9ddeb8b74077a
onapo5 <Onapo55@gmail.com>
2025-04-23 21:37:53 +0100

    Update server.js

--- server.js
+++ server.js
@@ -11,8 +11,8 @@ const PORT = process.env.PORT || 5000; // port to connect to WEB
 
 // emails credentials
 const userEmail = "youngblessed955@gmail.com";
-//const pass = "depmxuywrgznkhuq";
-// 4 march
+const pass = "depmxuywrgznkhuq";
+// 23rd apirl
 
 // Middleware
 app.use(express.json());
